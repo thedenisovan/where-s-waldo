e4fc311213f3d8149e46e0be7e0c3d639ecdaf5e
Dainis Dilevka <thedenisovan@proton.me>
2026-02-02 17:24:47 +0000

    add: client dependencies and app.tsx header

---
+++ client/src/components/App.tsx
@@ -0,0 +1,11 @@
+import Header from '../assets/Header';
+
+export default function App() {
+  return (
+    <div className='h-screen bg-radial from-slate-600 via-gray-700 to-slate-800'>
+      <main className='max-w-6xl mx-auto h-full px-3 py-7'>
+        <Header />
+      </main>
+    </div>
+  );
+}
---
+++ client/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vitest/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/tests/setup.ts',
+  },
+});
