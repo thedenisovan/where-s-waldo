27a06edcf5134832e475f1c672770c56d045ffcf
Dainis Dilevka <thedenisovan@proton.me>
2026-02-03 09:48:42 +0000

    add: header tests

--- client/src/tests/Header.test.tsx
+++ client/src/tests/Header.test.tsx
@@ -3,13 +3,19 @@ import { render, screen } from '@testing-library/react';
 import { logRoles } from '@testing-library/dom';
 import Header from '../assets/Header';
 
-it('Header component should be in the document', () => {
-  const { container } = render(<Header />);
+describe('Expect all header elements to be in document', () => {
+  it('Header, p and h1 to be in document', () => {
+    const { container } = render(<Header />);
 
-  logRoles(container); // HTMLElement
+    logRoles(container); // HTMLElement
 
-  expect(screen.getByRole('banner')).toBeInTheDocument();
-  expect(screen.getByRole('heading')).toHaveTextContent(
-    'Find the tiny character.',
-  );
+    expect(screen.getByRole('banner')).toBeInTheDocument();
+    // Paragraph element
+    expect(
+      screen.getByText(
+        'Tap/click the scene to guess. When you find all characters you win. No downloads no login.',
+      ),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('heading')).toBeInTheDocument();
+  });
 });
