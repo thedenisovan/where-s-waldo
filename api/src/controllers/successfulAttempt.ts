629caf9ea8bc141f42722da3712f53b6068857c4
Dainis Dilevka <thedenisovan@proton.me>
2026-02-13 13:03:13 +0000

    add: info of whihc characters are still not found in api response

--- api/src/controllers/successfulAttempt.ts
+++ api/src/controllers/successfulAttempt.ts
@@ -37,7 +37,12 @@ async function successfulAttempt(req: Request, res: Response) {
       return res.status(200).json({ message: 'COMPLETED' });
     }
 
-    return res.status(200).json({ message: 'ONGOING' });
+    return res.status(200).json({
+      message: 'HIT',
+      char1: updatedAttempt.char1Alive,
+      char2: updatedAttempt.char2Alive,
+      char3: updatedAttempt.char3Alive,
+    });
   } catch {
     res.status(500).json({ message: 'Internal database/server error' });
   }
